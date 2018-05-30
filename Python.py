8c9e0a6bec22bffd1ac05f63df21fe1dd19c71d5
thomaslast <git@github.com>
2018-05-30 22:53:28 +0100

    Test
    Signed-off-by: thomaslast <git@github.com>

--- Python.py
+++ Python.py
@@ -5,3 +5,5 @@ import numpy as np
 x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
 plt.plot(x/2, (np.sin(x)))       # Plot the sine of each x point
 plt.show()                   # Display the plot
+
+print("TESTTTTT")
